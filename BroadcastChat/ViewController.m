c73b57242bfbc7c756d37696c74513e0d68bb58b
0atme0 <0atme0@gmail.com>
2016-07-10 04:04:20 +0300

    Initial Commit

---
+++ BroadcastChat/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BroadcastChat
+//
+//  Created by Andrey Ildyakov on 10.07.16.
+//  Copyright © 2016 111. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
